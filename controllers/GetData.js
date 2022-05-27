fbad8b89079aca9e5f08f6e70148a761e810f633
adeputragea <adeputragea28@gmail.com>
2022-05-27 09:26:34 +0700

    use aggregate to find all data in GetData.js

--- controllers/GetData.js
+++ controllers/GetData.js
@@ -15,7 +15,16 @@ export const getAllDataEmployee = async (req, res) => {
 // this to get all data attendance
 export const getAllDataAttendance = async (req, res) => {
   try {
-    const getAll = await Attendance.find({});
+    const getAll = await Attendance.aggregate([
+      {
+        $lookup: {
+          from: 'employees',
+          localField: 'employeeId',
+          foreignField: '_id',
+          as: 'hasil',
+        },
+      },
+    ]);
     res.status(200).json(getAll);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -25,7 +34,16 @@ export const getAllDataAttendance = async (req, res) => {
 // this to get all data leave
 export const getAllDataLeave = async (req, res) => {
   try {
-    const getAll = await Leave.find({});
+    const getAll = await Leave.aggregate([
+      {
+        $lookup: {
+          from: 'employees',
+          localField: 'employeeId',
+          foreignField: '_id',
+          as: 'hasil',
+        },
+      },
+    ]);
     res.status(200).json(getAll);
   } catch (error) {
     res.status(500).json({ message: error.message });
---
+++ tes.js
@@ -0,0 +1,13 @@
+const employee = {
+    _id,
+    name,
+    attendance : {
+        status,
+        attendanceDate
+    },
+    leave: {
+        isApprove,
+        startDate,
+        endDate
+    }
+}
